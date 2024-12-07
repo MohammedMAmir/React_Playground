433cb5be7a0c6b80b76a3caba2effe2661b77beb
Mohammed <muji-amir@hotmail.com>
2024-12-07 18:51:13 -0500

    new boilerplate

---
+++ calc/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Wrapper from "./components/Wrapper";
+
+function App(){
+    return (
+        <div>
+            <Wrapper>
+                <h1>App</h1>
+            </Wrapper> 
+        </div>
+    )
+}
+
+export default App;
